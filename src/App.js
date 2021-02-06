1c69cfc27264db1e09ffacf6c649f8835a9a7327
Michael <michael.krontz@beckshybrids.com>
2021-02-06 13:07:06 -0500

    More Auth WIP

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
+import React from 'react';
 import './App.css';
 import galleryItem1 from './hp-hero-1.jpg';
 import galleryItem2 from './hp-hero-2.jpg';
 import galleryItem3 from './hp-hero-3.jpg';
 import galleryItem4 from './hp-hero-4.jpg';
 import galleryItem5 from './hp-hero-5.jpg';
-import { useRef } from 'react'
-import { Form, Button, Card, Container } from 'react-bootstrap'
-import { useAuth } from './AuthContext'
+import { Container } from 'react-bootstrap'
+import Signup from './Signup'
+import { AuthProvider } from './AuthContext'
 
-function Signup() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const passwordConfirmRef = useRef()
-  const { signupUser } = useAuth()
-
-  function handleSubit(e) {
-    e.preventDefault()
-
-    signupUser(emailRef.current.value, passwordRef.current.value)
-  }
-
-  return (
-    <>
-    <Card>
-      <Card.Body><h2 className="text-center mb-4">Sign Up</h2>
-        <Form>
-          <Form.Group id="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" ref={emailRef} required></Form.Control>
-          </Form.Group>
-          <Form.Group id="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" ref={passwordRef} required></Form.Control>
-          </Form.Group>
-          <Form.Group id="password-confirm">
-            <Form.Label>Password Confirmation</Form.Label>
-            <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
-          </Form.Group>
-          <Button className="w-100" type="submit">Sign Up</Button>
-        </Form>
-      </Card.Body>
-    </Card>
-    <div className="w-100 text-center mt-2"></div>
-  </>
-  )
-}
 
 function Logo() {
   return (
---
+++ src/Signup.js
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react'
+import { Form, Button, Card } from 'react-bootstrap'
+import { useAuth } from './AuthContext'
+
+
+export default function Signup() {
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef()
+    const { signupUser } = useAuth()
+  
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    signupUser(emailRef.current.value, passwordRef.current.value)
+  }
+  
+  return (
+    <>
+    <Card>
+      <Card.Body><h2 className="text-center mb-4">Sign Up</h2>
+        <Form>
+          <Form.Group id="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email" ref={emailRef} required></Form.Control>
+          </Form.Group>
+          <Form.Group id="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" ref={passwordRef} required></Form.Control>
+          </Form.Group>
+          <Form.Group id="password-confirm">
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
+          </Form.Group>
+          <Button className="w-100" type="submit">Sign Up</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+    <div className="w-100 text-center mt-2"></div>
+  </>
+  )
+};
+
+
+
+
+
