8b5d1b44470e82d6f3ed782e4403174d3f22d895
Michael <michael.krontz@beckshybrids.com>
2021-02-07 11:52:06 -0500

    auth routing

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import galleryItem5 from './hp-hero-5.jpg';
 import { Container } from 'react-bootstrap'
 import Signup from './Signup'
 import { AuthProvider } from './AuthContext'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
+import PrivateRoute from './PrivateRoute'
 
 
 function Logo() {
@@ -54,13 +58,19 @@ function App() {
         <NavBar></NavBar>
       </header>
 
-    <AuthProvider>
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
         <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Signup></Signup>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
-    </AuthProvider>
 
       <div className = "App-body">
         <ImageGallery></ImageGallery>
