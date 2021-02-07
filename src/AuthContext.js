ad3d6751972fc30ce7a07d12ae6625cd0c84ed50
Michael <michael.krontz@beckshybrids.com>
2021-02-07 10:09:45 -0500

    Signup complete

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -9,6 +9,7 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState()
+  const [loading, setLoading] = useState(true)
 
   function signupUser(email, password) {
     auth.createUserWithEmailAndPassword(email, password)
@@ -17,6 +18,7 @@ export function AuthProvider({ children }) {
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user)
+      setLoading(false)
     })
 
     return unsubscribe
@@ -29,7 +31,7 @@ export function AuthProvider({ children }) {
   
   return (
     <AuthContext.Provider value={value}>
-        {children}
+        {!loading && children}
     </AuthContext.Provider>
     
   )
