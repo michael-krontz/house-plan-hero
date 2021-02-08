817a17d56581b1cee6dee04171f10ab72f576c81
Michael <michael.krontz@beckshybrids.com>
2021-02-08 07:00:32 -0500

    finished authentication

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
 import Login from './Login'
 import PrivateRoute from './PrivateRoute'
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 
 function Logo() {
@@ -64,8 +66,10 @@ function App() {
             <AuthProvider>
               <Switch>
                 <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute path="/update-profile" component={UpdateProfile} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
               </Switch>
             </AuthProvider>
           </Router>
