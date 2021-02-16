0b00fd5ce14751bb23b14baa3190595ec1512a9a
Michael <michael.krontz@beckshybrids.com>
2021-02-16 08:02:56 -0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Container } from 'react-bootstrap'
-import Signup from './Signup'
-import { AuthProvider } from './AuthContext'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Dashboard from './Dashboard'
-import Login from './Login'
-import PrivateRoute from './PrivateRoute'
-import ForgotPassword from "./ForgotPassword"
-import UpdateProfile from "./UpdateProfile"
+// import { Container } from 'react-bootstrap'
+// import Signup from './Signup'
+// import { AuthProvider } from './AuthContext'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import Dashboard from './Dashboard'
+// import Login from './Login'
+// import PrivateRoute from './PrivateRoute'
+// import ForgotPassword from "./ForgotPassword"
+// import UpdateProfile from "./UpdateProfile"
+import { useSprings, animated, interpolate } from 'react-spring'
+import { useDrag } from 'react-use-gesture'
 
-function Logo() {
-  return (
-    <div className = "Logo-wrapper">
-      <h4 style={{ marginBottom: "0" }}>House Plan Hero</h4>      
-    </div>
-  );
-};
+let cards = []
 
-function NavBar() {
-  return (
-  <div className = "Nav-wrapper">
-      <ul className = "Nav">
-        <li>Login</li>
-      </ul>
-  </div>
-  );
-}
+// These two are just helpers, they curate spring data, values that are later being interpolated into css
+const to = i => ({ x: 0, y: i * -4, scale: 1, rot: -10 + Math.random() * 20, delay: i * 100 })
+const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
+// This is being used down there in the view, it interpolates rotation and scale into a css transform
+const trans = (r, s) => `perspective(1500px) rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
-function ApiCall() {
+function Deck() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
+  const [gone] = useState(() => new Set()) // The set flags all the cards that are flicked out
+  const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) })) // Create a bunch of springs using the helpers above
+  // Create a gesture, we're interested in down-state, delta (current-pos - click-pos), direction and velocity
+  const bind = useDrag(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
+    const trigger = velocity > 0.2 // If you flick hard enough it should trigger the card to fly out
+    const dir = xDir < 0 ? -1 : 1 // Direction should either point left or right
+    if (!down && trigger) gone.add(index) // If button/finger's up and trigger velocity is reached, we flag the card ready to fly out
+    set(i => {
+      if (index !== i) return // We're only interested in changing spring-data for the current spring
+      const isGone = gone.has(index)
+      const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0 // When a card is gone it flys out left or right, otherwise goes back to zero
+      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0) // How much the card tilts, flicking it harder makes it rotate faster
+      const scale = down ? 1.1 : 1 // Active cards lift up a bit
+      return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
+    })
+    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
+  })
 
   useEffect(() => {
     fetch("https://house-plan-hero-default-rtdb.firebaseio.com/houseplans.json")
@@ -56,47 +65,85 @@ function ApiCall() {
   } else if (!isLoaded) {
     return <div>Loading...</div>;
   } else {
-    return (
-      <ul>
-        {items.map(item => (
-          <li key={item.id}>
-            <img src={`../images/hph_${item.id}.jpg`}  alt={item.name}  className = "responsive"/>
-          </li>
-        ))}
-      </ul>
-    );
+
+    let itemarray =  [items.map(item => (
+
+      <li key={item.id}>
+        item = {item.id}
+      </li>
+
+  ))]
+
+  cards = [{ items} ]
+
+  console.log(itemarray);
+
+
+    // Now we're just mapping the animated values to our view, that's it. Btw, this component only renders once. :-)
+  return props.map(({ x, y, rot, scale }, i) => (
+    <animated.div key={i} style={{ transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`) }}>
+      {/* This is the card itself, we're binding our gesture to it (and inject its index so we know which is which) */}
+      <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans), backgroundImage: `url(${itemarray[i]})` }} />
+    </animated.div>
+  ))
   }
 }
 
+// function Logo() {
+//   return (
+//     <div className = "Logo-wrapper">
+//       <h4 style={{ marginBottom: "0" }}>House Plan Hero</h4>      
+//     </div>
+//   );
+// };
+
+// function NavBar() {
+//   return (
+//   <div className = "Nav-wrapper">
+//       <ul className = "Nav">
+//         <li>Login</li>
+//       </ul>
+//   </div>
+//   );
+// }
+
+
 function App() {
   return (
-    <div className="App">
 
-      <header className = "App-header">
-        <Logo></Logo>
-        <NavBar></NavBar>
-      </header>
+    <Deck></Deck>
 
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-        <div className="w-100" style={{ maxWidth: '400px' }}>
-          <Router>
-            <AuthProvider>
-              <Switch>
-                <PrivateRoute exact path="/" component={Dashboard} />
-                <PrivateRoute path="/update-profile" component={UpdateProfile} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/login" component={Login} />
-                <Route path="/forgot-password" component={ForgotPassword} />
-              </Switch>
-            </AuthProvider>
-          </Router>
-        </div>
-      </Container>
 
-      <div className = "App-body">
-        <ApiCall></ApiCall>
-      </div>
-    </div>
+    // <div className="App">
+
+
+
+
+    //   <header className = "App-header">
+    //     <Logo></Logo>
+    //     <NavBar></NavBar>
+    //   </header>
+
+    //   <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+    //     <div className="w-100" style={{ maxWidth: '400px' }}>
+    //       <Router>
+    //         <AuthProvider>
+    //           <Switch>
+    //             <PrivateRoute exact path="/" component={Dashboard} />
+    //             <PrivateRoute path="/update-profile" component={UpdateProfile} />
+    //             <Route path="/signup" component={Signup} />
+    //             <Route path="/login" component={Login} />
+    //             <Route path="/forgot-password" component={ForgotPassword} />
+    //           </Switch>
+    //         </AuthProvider>
+    //       </Router>
+    //     </div>
+    //   </Container>
+
+    //   <div className = "App-body">
+    //     <ApiCall></ApiCall>
+    //   </div>
+    // </div>
   );
 };
 
