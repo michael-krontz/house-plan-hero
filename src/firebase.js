8a9b075e11dbea1dec219e71ef7dc29d5f6a0866
michael-krontz <michaelkrontz@gmail.com>
2021-05-04 16:34:51 -0400

    api change

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import "firebase/auth"
 import "firebase/database"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDLZ7YC7RtDjQbcPDUiDXNB3EcA3itIUAU",
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
